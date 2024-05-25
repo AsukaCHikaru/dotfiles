2386a00ca061efd8016196121ae7e270b11fc8cf
asukachikaru <asukachikaru@gmail.com>
2024-05-25 12:03:12 +0900

    add: config.fish

---
+++ config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -x PATH node_modules/.bin $PATH
+
+function nvm
+    bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+end
+
+function __check_nvm --on-variable PWD --description 'Do nvm stuff'
+  if test -f .nvmrc
+    nvm use
+  end
+end
+
+__check_nvm
